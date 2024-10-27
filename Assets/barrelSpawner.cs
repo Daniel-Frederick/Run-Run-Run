603165118ad90a52b62057ba731e773f172395ec
Daniel Frederick <frederickd5@udayton.edu>
2024-10-26 20:12:06 -0400

    barrels will spawn from the right of the screen and will move to the left and destory once off screen

--- Assets/barrelSpawner.cs
+++ Assets/barrelSpawner.cs
@@ -4,27 +4,42 @@ public class barrelSpawner : MonoBehaviour
 {
     public LogicManager logicManager;
     public GameObject Barrel;
-    private float minLimit;
-    private float maxLimit;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 1;
 
     void Start() {
         
     }
 
     void Update() { // Fix the Spawn rates
-        if (logicManager.gameTimer <= 10) { // Check amout of time passed
-            if (logicManager.resetTimer >= logicManager.timerInterval +3) { // Inital spawn rate
-                spawner();
-            }
-        } else if (logicManager.gameTimer <= 20) {
-            if (logicManager.resetTimer >= logicManager.timerInterval) { // Increase spawn rate
-                spawner();
-            }
-        } else { // Max spawn rate
-            if (logicManager.resetTimer >= logicManager.timerInterval/2) {
-                spawner();
-            }
+        //if (logicManager.gameTimer <= 10) { // Check amout of time passed
+        //    if (logicManager.resetTimer >= logicManager.timerInterval) { // Inital spawn rate
+        //        spawnBarrel();
+        //    }
+        //} else if (logicManager.gameTimer <= 20) {
+        //    if (logicManager.resetTimer >= logicManager.timerInterval) { // Increase spawn rate
+        //        spawnBarrel();
+        //    }
+        //} else { // Max spawn rate
+        //    if (logicManager.resetTimer >= logicManager.timerInterval) {
+        //        spawnBarrel();
+        //    }
+        //}
+
+        if (timer < spawnRate) {
+            timer += Time.deltaTime;
+        } else {
+            spawnBarrel();
+            timer = 0;
         }
+
+
     }
-    private void spawner() { Instantiate(Barrel, transform.position, transform.rotation); }
+
+    private void spawnBarrel() {
+        float highestY = transform.position.y + heightOffset;
+        float lowestY = transform.position.y - heightOffset;
+        Instantiate(Barrel, new Vector3(transform.position.x, Random.Range(lowestY, highestY), 0) , transform.rotation);
+    }
 }
--- Assets/movement.cs
+++ Assets/movement.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float moveSpeed = 5;
+    private float deleteZone = -13;
+
     void Start() {
         
     }
 
     void Update() {
-
         transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+
+        if (transform.position.x < deleteZone) {
+            Destroy(gameObject);
+        }
     }
 }
